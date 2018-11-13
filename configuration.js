495ccab58b73243db12f6b8f86055bfbcbab6a21
Marc van Duivenvoorde <marc.vanduivenvoorde@synetic.nl>
2018-11-13 21:50:47 +0100

    - update stuff

---
+++ configuration.js
@@ -0,0 +1,19 @@
+var configuration = {
+  "showTracks": {
+    "_58f9d620b7c04b5db2c974e60b787da6": "Show all tracks",
+    "_f964c1931e334aa4a22e6eafee8be15f": "Show all drum tracks",
+    "_828c84e70eb94f4eb85f9e3b558a4684": "Show all guitar tracks",
+    "_9acf612969bf4490b0914f55b9b8fa7a": "Show all bass tracks"
+  },
+
+  "screensets": {
+    40454: "Dual screen mixing"
+  },
+
+  "customActions": {
+    "_32d8ba1928434a54a9369ab05313ddfe": "cut/edit racktom 1",
+    "_cc629bc752474212be6587f9be1366e9": "cut/edit racktom 2",
+    "_401d624717c34baeb34299eaaca3c95d": "cut/edit floortom 1",
+    "_d47116668d404d88839648c0bda78b62": "cut/edit floortom 2",
+  }
+};
